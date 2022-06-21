b455d6559f2f64f9411c56f6e12fa733f97a9b40
dudushy <eduardotalarico1337@gmail.com>
2022-06-21 13:59:47 -0300

    First working version

--- src/main.py
+++ src/main.py
@@ -20,7 +20,27 @@ def calculateIncome() -> None:
     hours = int(input("\nPlease enter your work daily hours. (int)\n>>> ")) #TODO try|except
     days = int(input("\nPlease enter how many days in the week you work. (int)\n>>> ")) #TODO try|except
     
-    print(f"\nincome: {income}\nhours: {hours}\ndays: {days}")
+    print(f"\nincome: {income}")
+    print(f"hours: {hours}")
+    print(f"days: {days}")
+    
+    perHour = income / ((days * 4) * hours)
+    
+    perDay = perHour * hours
+
+    perYear = income * 12
+    
+    # print(f"\nperHour: {perHour}")
+    print("\nperHour: ${:0,.2f}".format(perHour))
+    
+    # print(f"perDay: {perDay}")
+    print("perDay: ${:0,.2f}".format(perDay))
+    
+    # print(f"perYear: {perYear}")
+    print("perYear: ${:0,.2f}".format(perYear))
+
+    # print(f"perYear*: {perYear + income} (Christmas bonus)")
+    print("perYear*: ${:0,.2f} (Christmas bonus)".format(perYear + income))
 
 def main() -> None:
     clearConsole()
