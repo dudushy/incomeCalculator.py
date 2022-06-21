f4b4c3ce7d6e6735870a23817ce66d7c441939b8
dudushy <eduardotalarico1337@gmail.com>
2022-06-21 14:41:11 -0300

    Loop, try and return

--- src/main.py
+++ src/main.py
@@ -13,16 +13,8 @@ perYear = 0
 def clearConsole() -> None: #? Clear console
     os.system("cls" if os.name == "nt" else "clear")
     
-def calculateIncome() -> None:
+def calculateIncome(income:float, hours:int, days:int) -> dict:
     global perHour, perDay, perYear
-    
-    income = float(input("Please enter your monthly income. (float)\n>>> ")) #TODO try|except
-    hours = int(input("\nPlease enter your work daily hours. (int)\n>>> ")) #TODO try|except
-    days = int(input("\nPlease enter how many days in the week you work. (int)\n>>> ")) #TODO try|except
-    
-    print(f"\nincome: {income}")
-    print(f"hours: {hours}")
-    print(f"days: {days}")
     
     perHour = income / ((days * 4) * hours)
     
@@ -30,21 +22,34 @@ def calculateIncome() -> None:
 
     perYear = income * 12
     
-    # print(f"\nperHour: {perHour}")
-    print("\nperHour: ${:0,.2f}".format(perHour))
-    
-    # print(f"perDay: {perDay}")
-    print("perDay: ${:0,.2f}".format(perDay))
-    
-    # print(f"perYear: {perYear}")
-    print("perYear: ${:0,.2f}".format(perYear))
-
-    # print(f"perYear*: {perYear + income} (Christmas bonus)")
-    print("perYear*: ${:0,.2f} (Christmas bonus)".format(perYear + income))
+    return {
+        "perHour": perHour,
+        "perDay": perDay,
+        "perYear": perYear,
+        "perYear*": perYear + income
+    }
 
 def main() -> None:
-    clearConsole()
-    calculateIncome()
+    while True:
+        clearConsole()
+        try:
+            income = float(input("Please enter your monthly income. (float)\n>>> "))
+            hours = int(input("\nPlease enter your work daily hours. (int)\n>>> "))
+            days = int(input("\nPlease enter how many days in the week you work. (int)\n>>> "))
+        except Exception as e:
+            print(f"\nERROR: {e}\nPlease insert the right value!\n")
+            os.system("pause")
+            continue
+        
+        output = calculateIncome(income, hours, days)
+        
+        print("\n- - - - - - - - - - - - - - - - - - - - - - - -\n")
+        for item in output:
+            print(f"{item}: " + "${:0,.2f}\n".format(output[item]))
+        print("- - - - - - - - - - - - - - - - - - - - - - - -\n")
+        
+        if input("Run again? (y/n)\n>>> ").lower() == "n":
+            break
 
 #! Main
 if __name__ == "__main__":
